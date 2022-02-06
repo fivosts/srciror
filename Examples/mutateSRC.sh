3fc2e4da2eebf3eb290aa52ff5b1b341e68e000a
Foivos Tsimpourlas <fivos_ts@hotmail.com>
2022-02-06 22:30:26 +0200

    Update mutateSRC.sh

--- Examples/mutateSRC.sh
+++ Examples/mutateSRC.sh
@@ -10,4 +10,4 @@ echo "$CURR_DIR/test.c:3,4,5,6,7,8,9" > ~/.srciror/coverage
 export SRCIROR_LLVM_BIN=$CURR_DIR/../llvm-build/Release+Asserts/bin
 export SRCIROR_LLVM_INCLUDES=$CURR_DIR/../llvm-build/Release+Asserts/lib/clang/3.8.0/include
 export SRCIROR_SRC_MUTATOR=$CURR_DIR/../SRCMutation/build/mutator
-python $CURR_DIR/../PythonWrappers/mutationClang $CURR_DIR/test.c -o test
+python $CURR_DIR/../PythonWrappers/mutationClang $@ $CURR_DIR/test.c -o test
